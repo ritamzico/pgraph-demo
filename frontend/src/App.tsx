450d53786dffd3eabb9da8066f66938328b5a8e1
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2026-03-17 15:53:15 -0700

    changed from using next analytics to react analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import { GraphCanvas } from './components/GraphCanvas';
 import { GraphSelector } from './components/GraphSelector';
 import { QueryPanel } from './components/QueryPanel';
